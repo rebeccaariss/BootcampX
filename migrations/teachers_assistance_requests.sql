18c1913e6ab73f661d9877694fe140b8ad7b945c
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-08-02 11:25:43 -0400

    Add missing column to assistance_requests table creation; add data for teachers and assistance_requests

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -12,6 +12,7 @@ CREATE TABLE assistance_requests (
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
+  started_at TIMESTAMP,
   completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
